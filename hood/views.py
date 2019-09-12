7f6b5ab812f22fcdfadd0af24d199ad799cffbcb
Sunday Brian <brians931@gmail.com>
2019-09-12 21:39:13 +0300

    add base html

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,redirect
+from django.http import Http404,HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse ("<h1>Hoodlum</h1>")
