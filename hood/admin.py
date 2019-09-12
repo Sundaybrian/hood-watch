36db39e08924222cba7134e55b06a1aefe99143e
Sunday Brian <brians931@gmail.com>
2019-09-12 22:32:51 +0300

    register the classes to the admin.py

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+import * from .models
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Occupant)
+admin.site.register(Neighbourhood)
+
