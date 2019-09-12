b7439fea114b5a3d63305b5efbfc0753a986e736
Sunday Brian <brians931@gmail.com>
2019-09-12 22:30:27 +0300

    add the str magic methods for the differnt classes

--- hood/models.py
+++ hood/models.py
@@ -13,16 +13,22 @@ class NeighbourHood(models.Model):
     occupants=models.IntegerField(blank=True,default=0)
     admin=models.ForeignKey(User,on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return f'Hood-{self.name}'
+
 class Post(models.Model):
-    '''
-    
+    '''  
     '''
     title=models.CharField(max_length=30)
     description=models.TextField()
     image=models.ImageField(upload_to='posters/',default='')
     date_posted=models.DateTimeField(auto_now_add=True)
-    last_updated=models.DateTimeField(auto_now_add=timezone)
+    last_updated=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'Post-{self.title}'
      
 
 class Business(models.Model):
@@ -30,12 +36,18 @@ class Business(models.Model):
     location=models.CharField(max_length=60)
     email=models.EmailField(blank=True,null=True)
     owner=models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    hood=models.ForeignKey(NeighbourHood,on_delete=models.DO_NOTHING)
+    hoodbizID=models.ForeignKey(NeighbourHood,on_delete=models.DO_NOTHING)
 
 
+    def __str__(self):
+        return f'Business-{self.name}'
 
-class Occupants(models.Model):
-    occupants=models.IntegerField(blank=True,default=0)
-    hood=models.ForeignKey(NeighbourHood,on_delete=models.CASCADE)
+class Occupant(models.Model):
+    occupants_num=models.IntegerField(blank=True,default=0)
+    hoodID=models.ForeignKey(NeighbourHood,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'Occupants-{self.occupants_num}-{self.hoodID.name}'
 
 
