386a6bfecebcf9e7fac8cdae86b37cab731e06c0
Sunday Brian <brians931@gmail.com>
2019-09-12 22:07:28 +0300

    add the neighbourhood class and occupants class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+
 
 # Create your models here.
+class NeighbourHood(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.CharField(max_length=60)
+    occupants=models.IntegerField(blank=True,default=0)
+    admin=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+
+class Business(models.Model):
+
+
+
+class Occupants(models.Model):
+    occupants=models.IntegerField(blank=True,default=0)
+    hood=models.ForeignKey(NeighbourHood,on_delete=models.CASCADE)
+
+
