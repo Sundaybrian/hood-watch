e3378446da23606e49f89867b97bc4d267e45759
Sunday Brian <brians931@gmail.com>
2019-09-13 15:05:12 +0300

    add the url for post detail class view

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PostDetailView
 
 urlpatterns=[
     path('',views.home,name='hood-home'),
+    path('post/<int:pk>',PostDetailView.as_view(),name='post-detail'),
 ]
