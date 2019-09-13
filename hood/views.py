7a7469c8a8b68a4c267522ec613416123cf91a35
Sunday Brian <brians931@gmail.com>
2019-09-13 12:22:54 +0300

    add the initial imports for the hood views.py

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render,get_list_or_404,redirect
 from django.http import Http404,HttpResponse
+from django.contrib import ,messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import (UserPassesTestMixin,LoginRequiredMixin)
+from django.views.generic import(ListView,DeleteView,DeleteView,CreateView,UpdateView)
+from django.contrib.auth.models import User
+from .models import Business,NeighbourHood,Location,Post,ObjectDoesNotExist
+
 
 # Create your views here.
 
