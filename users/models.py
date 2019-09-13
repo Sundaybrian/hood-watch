07d3063dbd85c016b2f4a92ab19da4f51a874fbe
Sunday Brian <brians931@gmail.com>
2019-09-13 09:30:49 +0300

    fix the signals for profiles not working

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     image=models.ImageField(default='naomi.jpg',upload_to='profile_pics')
     bio=models.TextField(blank=True)
-    neighbourhood=models.ForeignKey(NeighbourHood,on_delete=models.DO_NOTHING)
+    neighbourhood=models.ForeignKey(NeighbourHood,on_delete=models.DO_NOTHING,null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
