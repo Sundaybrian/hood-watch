d56291fec87585f7fd56743c69b9d1f79fd0f715
Sunday Brian <brians931@gmail.com>
2019-09-13 11:50:01 +0300

    override the save method for the profile model to resize the profile images

--- users/models.py
+++ users/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
 from hood.models import NeighbourHood,Business
+from PIL import Image
 # Create your models here.
+
 
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     image=models.ImageField(default='naomi.jpg',upload_to='profile_pics')
     bio=models.TextField(blank=True)
     neighbourhood=models.ForeignKey(NeighbourHood,on_delete=models.DO_NOTHING,null=True)
-    
+
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self,*args,**kwargs):
+        '''
+        overriding the save method inorder to resize the profile images
+        '''
+
+        super(Profile,self).save(*args,**kwargs)
+        img=Image.open(self.image)
+
+        if img.height>300 or img.width>300:
+            output_size=(300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)    
+
