7e4ac7a4bdb04d3ec11bc05f5182b70f0f16960a
Sunday Brian <brians931@gmail.com>
2019-09-13 11:06:00 +0300

    add more loginc fr the profile view function to render the two forms for profile and user details

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -22,7 +22,33 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    '''
+    view function for a user profile
+    '''
+
+    if request.method=='POST':
+        user_form=UserUpdateForm(request.POST,instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request,f'Account has been updated')
+
+            return redirect('profile')
+
+    else:
+        user_form=UserUpdateForm(instance=request.user)
+        profile_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+        'usr_form':user_form,
+        'prof_form':profile_form
+    }    
+
+
+
+    return render(request,'users/profile.html',context)
 
 
 
