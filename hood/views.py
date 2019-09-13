f2fdf87617338753231db085278f3af40888e36c
Sunday Brian <brians931@gmail.com>
2019-09-13 14:26:23 +0300

    add the home template to render the posts

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_list_or_404,redirect
 from django.http import Http404,HttpResponse
-from django.contrib import ,messages
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import (UserPassesTestMixin,LoginRequiredMixin)
 from django.views.generic import(ListView,DeleteView,DeleteView,CreateView,UpdateView)
