e674b5c406a93014ce6b117be4f7dbc313f8c9e8
Sunday Brian <brians931@gmail.com>
2019-09-13 15:28:08 +0300

    add the view for the post create view class

--- hood/urls.py
+++ hood/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import PostDetailView
+from .views import (PostDetailView,PostCreateView)
 
 urlpatterns=[
     path('',views.home,name='hood-home'),
+    path('post/new/',PostCreateView.as_view(),name='post-new'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
 ]
