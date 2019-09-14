b1f2073d4c8d98e825ab666e8bcb3df1c3f7cdb2
Sunday Brian <brians931@gmail.com>
2019-09-14 14:18:56 +0300

    add the url for the post delete view

--- hood/urls.py
+++ hood/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import (PostDetailView,PostCreateView,PostUpdateView)
+from .views import (PostDetailView,PostCreateView,PostUpdateView,PostDeleteView)
 
 urlpatterns=[
     path('',views.home,name='hood-home'),
     path('post/new/',PostCreateView.as_view(),name='post-new'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-new'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
 ]
